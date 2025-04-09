6ce440b720eb33cfe6afa1961c9e5b3c63f4c3c8
mmaster <myles1551@gmail.com>
2025-04-08 23:55:58 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,43 @@ bool checkWhitespace(const std::string& s) {
 	return true;
 }
 
+void checkEntry(const std::filesystem::directory_entry& entry, int* total, int* whitespace) {
+	if (entry.path().has_extension()) {
+		std::string filename = entry.path().generic_string();
+
+		std::cout << "Checking file: " << filename << "\n";
+
+		std::ifstream inFile(filename);
+		std::string line;
+		while (std::getline(inFile, line)) {
+			if (checkWhitespace(line)) {
+				*whitespace += 1;
+			}
+			*total += 1;
+		}
+		inFile.close();
+	}
+}
+
 int main(int argc, char* argv[]) {
 
-	if (argc != 2) {
-		std::cout << "ERROR: Expected 1 command line parameter (PATH), got: " << argc-1 << ". \n";
+	if (argc < 2) {
+		std::cout << "error, not enough arguments\n";
 		return -1;
 	}
 
 	std::string dir = argv[1];
+
+	bool recursive = false;
+	std::string recursiveString = "";
+
+	if (argc > 2) {
+		std::string flag = argv[2];
+		if (flag == "-r") {
+			recursive = true;
+			recursiveString = " RECURSIVELY";
+		}
+	}
 
 	fs::path toCheck = fs::current_path() / dir;
 
@@ -31,34 +60,25 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 	
-	std::cout << "Checking contents of: " << toCheck.relative_path() << " directory. \n";
+	std::cout << "Checking contents of: " << toCheck.relative_path() << " directory" << recursiveString << ". \n";
 
 	int whitespace = 0;
 	int total = 0;
 
-	for (auto entry : fs::directory_iterator(toCheck)) {
-		if (entry.path().has_extension()) {
-			std::string filename = dir + entry.path().filename().string();
-
-			std::cout << "Checking file: " << filename << "\n";
-
-			std::ifstream inFile(filename);
-			std::string line;
-			while (std::getline(inFile, line)) {
-				if (checkWhitespace(line)) {
-					whitespace++;
-				}
-				total++;
-			}
-			inFile.close();
+	if (recursive) {
+		for (auto entry : fs::recursive_directory_iterator(toCheck)) {
+			checkEntry(entry, &total, &whitespace);
+		}
+	}
+	else {
+		for (auto entry : fs::directory_iterator(toCheck)) {
+			checkEntry(entry, &total, &whitespace);
 		}
 	}
 	
 	std::cout << "Total LOC: " << total << "\n";
 	std::cout << "Total lines of whitespace: " << whitespace << "\n";
 	std::cout << "Total LOC - whitespace: " << total-whitespace << "\n";
-
-
 
 	return 0;
 }
